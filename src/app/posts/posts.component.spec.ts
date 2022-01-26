78355b23b32cc339f92e2a2307d5db718aa0d0a2
PNik1ta <75731861+PNik1ta@users.noreply.github.com>
2022-01-26 19:51:17 +0400

    Add example of integration tests - done!

---
+++ src/app/posts/posts.component.spec.ts
@@ -0,0 +1,42 @@
+import { HttpClientModule } from '@angular/common/http';
+import { TestBed, ComponentFixture, async } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { PostsComponent } from "./posts.component";
+import { PostsService } from "./posts.service";
+
+describe('PostsComponent', () => {
+	let fixture: ComponentFixture<PostsComponent>
+	let component: PostsComponent
+	let service: PostsService
+
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			declarations: [PostsComponent],
+			providers: [PostsService],
+			imports: [HttpClientModule]
+		});
+
+		fixture = TestBed.createComponent(PostsComponent);
+		component = fixture.componentInstance;
+		//service = fixture.debugElement.injector.get(PostsService);
+		service = TestBed.get(PostsService);
+	});
+
+	xit('should fetch posts on ngOnInit', () => {
+		const posts = [1, 2, 3];
+
+		spyOn(service, 'fetch').and.returnValue(of(posts));
+		fixture.detectChanges();
+		expect(component.posts).toEqual(posts);
+	});
+
+	it('should fetch posts on ngOnInit (promise)', async(() => {
+		const posts = [1, 2, 3];
+		spyOn(service, 'fetchPromise').and.returnValue(Promise.resolve(posts));
+
+		fixture.detectChanges();
+		fixture.whenStable().then(() => {
+			expect(component.posts.length).toEqual(posts.length);
+		})
+	}));
+})
